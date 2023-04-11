bf8a55a08e89e04f6d75dc0129603958344e449a
nurlanahasanzada17 <6ea7n3z@code.edu.az>
2023-04-11 23:33:24 +0400

    hello

---
+++ script.js
@@ -0,0 +1,10 @@
+class Fish {
+    constructor(brand, model, currentFuel, fulFor1Km, millage) {
+        this.brand = brand;
+        this.model = model;
+        this.currentFuel = currentFuel;
+        this.fulFor1KM = fulFor1Km;
+        this.millage = millage;
+    }
+   
+}
